7ed72e5d8e5b07214e1bcbd2a9a65f146ae89ae2
Clément Vion <clement.vion49@gmail.com>
2017-03-19 16:07:59 +0100

    feat(router) add hashHistory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import HomeScene from './scenes/HomeScene'
 import ProjectScene from './scenes/ProjectScene'
 import './App.css'
